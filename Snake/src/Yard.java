a0c2a9ab6d71a5ddcaca0658647ee65ebfa0d125
hahn <hahn2017@my.yorku.ca>
2020-01-14 19:59:00 -0500

    build windows 2

--- Snake/src/Yard.java
+++ Snake/src/Yard.java
@@ -1,2 +1,35 @@
-public class Yard {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+//next paint
+
+public class Yard extends Frame{
+
+    private static final int ROWS = 100;
+    private static final int COLUMES = 100;
+    private static final int BLOCK_SIZE = 5;
+
+    //Image offScreenImage = null;
+
+
+    public void launch(){
+        this.setLocation(300,300);
+        this.setSize(COLUMES*BLOCK_SIZE,ROWS*BLOCK_SIZE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setVisible(true);
+    }
+
+
+
+    public static void main(String[] args){
+        new Yard().launch();
+
+    }
+
 }
