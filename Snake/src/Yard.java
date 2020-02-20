7dd8682cb4b4c2640b6a907e7b341257ee16da8a
hahn <hahn2017@my.yorku.ca>
2020-02-19 23:08:31 -0500

    draw boxes

--- Snake/src/Yard.java
+++ Snake/src/Yard.java
@@ -6,16 +6,18 @@ import java.awt.event.WindowEvent;
 
 public class Yard extends Frame{
 
-    private static final int ROWS = 100;
-    private static final int COLUMES = 100;
-    private static final int BLOCK_SIZE = 5;
+    private static final int ROWS = 50;
+    private static final int COLUMNS = 50;
+    private static final int BLOCK_SIZE = 10;
 
     //Image offScreenImage = null;
 
 
+
+
     public void launch(){
         this.setLocation(300,300);
-        this.setSize(COLUMES*BLOCK_SIZE,ROWS*BLOCK_SIZE);
+        this.setSize(COLUMNS*BLOCK_SIZE,ROWS*BLOCK_SIZE);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -32,4 +34,20 @@ public class Yard extends Frame{
 
     }
 
+    @Override
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.gray);
+        g.fillRect(0,0,COLUMNS*BLOCK_SIZE,ROWS*BLOCK_SIZE);
+        g.setColor(Color.DARK_GRAY);
+        //print the line
+        for(int i=1;i<ROWS; i++){
+            g.drawLine(0,BLOCK_SIZE*i,COLUMNS*BLOCK_SIZE,BLOCK_SIZE*i);
+        }
+        //print columns
+        for(int i=1;i<COLUMNS; i++){
+            g.drawLine(BLOCK_SIZE*i,0,BLOCK_SIZE*i,ROWS*BLOCK_SIZE);
+        }
+        g.setColor(c);
+    }
 }
