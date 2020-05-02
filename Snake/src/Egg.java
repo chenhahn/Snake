bc284458c6dfd16e061c64de178f99983d62d764
hahn <hahn2017@my.yorku.ca>
2020-05-02 12:39:32 -0400

    basic functions

--- Snake/src/Egg.java
+++ Snake/src/Egg.java
@@ -1,2 +1,50 @@
+import java.awt.*;
+import java.util.Random;
+
 public class Egg {
+    int row, col;
+    int w = Yard.BLOCK_SIZE;
+    int h = Yard.BLOCK_SIZE;
+    private static Random r = new Random();
+
+    public Egg(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Egg(){
+        this(r.nextInt(Yard.ROWS-3)+3, r.nextInt(Yard.COLUMNS));
+    }
+
+    public void reAppear(){
+        this.row = r.nextInt(Yard.ROWS-3)+3;
+        this.col = r.nextInt(Yard.COLUMNS);
+    }
+
+    public Rectangle getRect(){
+        return new Rectangle(Yard.BLOCK_SIZE*col, Yard.BLOCK_SIZE*row, w, h);
+    }
+
+    public void draw(Graphics g){
+        Color c = g.getColor();
+        g.setColor(Color.GREEN);
+        g.fillOval(Yard.BLOCK_SIZE*col, Yard.BLOCK_SIZE*row, w, h);
+        g.setColor(c);
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
 }
--- Snake/src/Yard.java
+++ Snake/src/Yard.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -6,11 +8,26 @@ import java.awt.event.WindowEvent;
 
 public class Yard extends Frame{
 
-    private static final int ROWS = 50;
-    private static final int COLUMNS = 50;
-    private static final int BLOCK_SIZE = 10;
+    private boolean flag = true;
 
-    //Image offScreenImage = null;
+    public static final int ROWS = 30;
+    public static final int COLUMNS = 30;
+    public static final int BLOCK_SIZE = 15;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    private int score = 0;
+
+    private Snake s = new Snake(this);
+    private Egg e = new Egg();
+
+    Image offScreenImage = null;
 
 
 
@@ -25,13 +42,18 @@ public class Yard extends Frame{
             }
         });
         this.setVisible(true);
+        this.addKeyListener(new KeyMonitor());
+        new Thread(new PaintThread()).start();
     }
 
 
 
     public static void main(String[] args){
         new Yard().launch();
+    }
 
+    public void stop(){
+        flag = false;
     }
 
     @Override
@@ -48,6 +70,50 @@ public class Yard extends Frame{
         for(int i=1;i<COLUMNS; i++){
             g.drawLine(BLOCK_SIZE*i,0,BLOCK_SIZE*i,ROWS*BLOCK_SIZE);
         }
+
+        g.setColor(Color.yellow);
+        g.drawString("score:" + score, 10, 60);
         g.setColor(c);
+        s.eat(e);
+        e.draw(g);
+        s.draw(g);
+
+        if(flag == false){
+            g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+            g.drawString("game over", 10, 80);
+        }
+    }
+
+    @Override
+    public void update(Graphics g) {
+        if(offScreenImage==null){
+            offScreenImage = this.createImage(COLUMNS*BLOCK_SIZE,ROWS*BLOCK_SIZE);
+        }
+        Graphics gOff = offScreenImage.getGraphics();
+        paint(gOff);
+        g.drawImage(offScreenImage, 0, 0, null);
+    }
+
+    private class PaintThread implements Runnable{
+
+        @Override
+        public void run() {
+            while(flag){
+                repaint();
+                try {
+                    Thread.sleep(100);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private class KeyMonitor extends KeyAdapter{
+
+        @Override
+        public void keyPressed(KeyEvent k) {
+            s.keyPressed(k);
+        }
     }
 }
